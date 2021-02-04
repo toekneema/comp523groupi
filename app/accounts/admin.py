b1201affe4d46aadc11593f0d5b162974e01ef3d
toekneema <officialtonyma@gmail.com>
2021-02-04 14:04:37 -0500

    created some very basic SQL table schemas

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Organization)
+admin.site.register(Patient)
+admin.site.register(Diagnosis)
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    #organizationID
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
+    #patientID
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Diagnosis(models.Model):
+    #organizationID
+    #patientID
+    result = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
